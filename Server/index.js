07a2f004017b0830e0698d3231bc0b550e420d37
amar <adyy2014@gmail.com>
2021-06-05 14:04:06 +0530

    updated

--- Server/index.js
+++ Server/index.js
@@ -10,12 +10,19 @@ const users = require('../model/userSchema')
 const bycrypt = require('bcryptjs')
 const photo = require('../model/photoSchema')
 const multer = require('multer')
+const session = require("express-session")
+
+app.use(session({
+    secret: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.use(cors())
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
 app.set('views', path.join(__dirname, '../views'))
-app.use(express.static(path.join(__dirname,'../public')))
+app.use(express.static(path.join(__dirname, '../public')))
 app.engine('hbs', exhandlebar({
     extname: 'hbs',
     defaultLayout: 'main'
@@ -27,22 +34,28 @@ app.get('/', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    users.findOne({email:req.body.email},(error,data)=>{
-        if(error){res.send("somethings worng")}
-        if(!data){
-            res.send("no email found, please register")
-        }
+    if (req.session.email) {
+        res.redirect('/home')
+    }
+    else {
+        users.findOne({ email: req.body.email }, (error, data) => {
+            if (error) { res.send("somethings worng") }
+            if (!data) {
+                res.send("no email found, please register")
+            }
 
-        else{
-            const hasspass=bycrypt.compareSync(req.body.password, data.password)
-            if(hasspass){
-                res.redirect('/home')
+            else {
+                const hasspass = bycrypt.compareSync(req.body.password, data.password)
+                if (hasspass) {
+                    req.session.email = req.body.email;
+                    res.redirect('/home')
+                }
+                else {
+                    res.send("wrong password")
+                }
             }
-            else{
-                res.send("wrong password")
-            }
-        }
-    })
+        })
+    }
 })
 
 var passport = require('passport')
@@ -88,7 +101,7 @@ app.get('/google/callback',
     function (req, res) {
         // Successful authentication, redirect success.
         res.redirect('/home');
-});
+    });
 
 app.get('/signup', (req, res) => {
     res.render('signup')
@@ -118,36 +131,41 @@ app.post('/signup', (req, res) => {
 })
 
 app.get('/home', (req, res) => {
-    photo.find({},(err,user)=>{
+    photo.find({}, (err, user) => {
         if (err) throw err
-        res.render('home',{
-            list:user
-        })
-        
+        if (req.session.email) {
+            res.render('home', {
+                email: req.session.emai,
+                list: user
+            })
+        } else {
+            res.send("Unauthorize User")
+        }
+
     }).lean()
 })
 
 app.get('/delete/:id', (req, res) => {
-    photo.findByIdAndDelete(req.params.id,(err,txt)=>{
-        if (!err){
+    photo.findByIdAndDelete(req.params.id, (err, txt) => {
+        if (!err) {
             res.redirect('/home')
         }
     })
 })
 
-app.get('/update/:_id', (req,res)=>{
-    photo.findOne({ _id: req.params._id},(err,user)=>{
-        if(!err){
-            res.render('update',{
-                ulist:user
+app.get('/update/:_id', (req, res) => {
+    photo.findOne({ _id: req.params._id }, (err, user) => {
+        if (!err) {
+            res.render('update', {
+                ulist: user
             })
         }
     }).lean()
 })
 
-app.post('/updateAll', (req,res)=>{
-    photo.findOneAndUpdate({_id:req.body._id}, req.body, {new:true}, (err,data)=>{
-        if(!err) {
+app.post('/updateAll', (req, res) => {
+    photo.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, data) => {
+        if (!err) {
             res.redirect('/home')
         }
     })
@@ -181,8 +199,15 @@ app.post('/add', upload.single('image'), (req, res) => {
 
 })
 
-app.get('/logout', (req,res)=>{
-    res.redirect('/')
+app.get('/logout', (req, res) => {
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+            res.send("Error")
+        } else {
+            res.render('login', { title: "Express", logout: "logout Successfully...!" })
+        }
+    })
 })
 
 app.get('/forgot', (req, res) => {
